04b229454d8aea78ed22ad5850599b4a1e0fe6b5
vidushidesai5 <60893700+vidushidesai5@users.noreply.github.com>
2020-03-01 18:26:01 -0500

    Add files via upload

---
+++ scripts/launch.bat
@@ -0,0 +1,4 @@
+@echo off
+CALL C:\Bitnami\WORDPR~1.2-3\scripts\setenv.bat
+START /MIN "Bitnami WordPress Stack Environment" CMD /C %*
+                    
---
+++ scripts/setenv.bat
@@ -0,0 +1,19 @@
+set PATH=C:\Bitnami\wordpress-5.3.2-3/apps/wordpress/bin;C:\Bitnami\wordpress-5.3.2-3/sqlite;C:\Bitnami\wordpress-5.3.2-3\imagemagick;C:/Bitnami/wordpress-5.3.2-3/php;C:\Bitnami\wordpress-5.3.2-3/mysql\bin;C:\Bitnami\wordpress-5.3.2-3/letsencrypt/;C:\Bitnami\wordpress-5.3.2-3/common\bin;%PATH%
+
+rem ##### SQLITE ENV #####
+			
+rem ##### GHOSTSCRIPT ENV #####
+set GS_LIB=C:\Bitnami\wordpress-5.3.2-3/common\lib
+rem ##### IMAGEMAGICK ENV #####
+set MAGICK_HOME=C:\Bitnami\wordpress-5.3.2-3\imagemagick
+
+set MAGICK_CODER_MODULE_PATH=C:\Bitnami\wordpress-5.3.2-3\imagemagick\modules\coders
+rem ##### PHP ENV #####
+set PHPRC=C:/Bitnami/wordpress-5.3.2-3/php
+rem ##### MYSQL ENV #####
+
+rem ##### APACHE ENV #####
+
+rem ##### SSL ENV #####
+set OPENSSL_CONF=C:\Bitnami\wordpress-5.3.2-3/apache2/conf\openssl.cnf
+
